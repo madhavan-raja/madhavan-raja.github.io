8be2f1efb117211775faa201dfadd80aad476c12
madhavan-raja <madhavanraja99@gmail.com>
2021-12-22 10:19:23 +0530

    Modified the theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,28 @@ module.exports = {
     extend: {
       screens: {
         'print': {'raw': 'print'},
+      },
+      textColor: {
+        light: {
+          base: 'var(--color-text-base)',
+          muted: 'var(--color-text-muted)',
+          link: 'var(--color-text-link)',
+          'link-hover': 'var(--color-text-link-hover)',
+        },
+        dark: {
+          base: 'var(--color-text-base-inverted)',
+          muted: 'var(--color-text-muted-inverted)',
+          link: 'var(--color-text-link-inverted)',
+          'link-hover': 'var(--color-text-link-hover-inverted)',
+        }
+      },
+      backgroundColor: {
+        light: {
+          fill: 'var(--color-fill)',
+        },
+        dark: {
+          fill: 'var(--color-fill-inverted)',
+        }
       }
     },
   },
