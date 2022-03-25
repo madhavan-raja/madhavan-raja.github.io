ff00412cea0749ebe1364deb1ae6923ae8986f74
madhavan-raja <madhavanraja99@gmail.com>
2022-03-25 19:06:34 +0530

    Changed selection color

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-           'sans': ['Poppins'],
+        'sans': ['Poppins'],
       },
       screens: {
         'print': {'raw': 'print'},
@@ -19,20 +19,24 @@ module.exports = {
           muted: 'var(--color-text-muted)',
           link: 'var(--color-text-link)',
           'link-hover': 'var(--color-text-link-hover)',
+          'highlight-fg': 'var(--color-text-highlight-fg)',
         },
         dark: {
           base: 'var(--color-text-base-inverted)',
           muted: 'var(--color-text-muted-inverted)',
           link: 'var(--color-text-link-inverted)',
           'link-hover': 'var(--color-text-link-hover-inverted)',
+          'highlight-fg': 'var(--color-text-highlight-fg-inverted)',
         }
       },
       backgroundColor: {
         light: {
           fill: 'var(--color-fill)',
+          'highlight-bg': 'var(--color-text-highlight-bg)',
         },
         dark: {
           fill: 'var(--color-fill-inverted)',
+          'highlight-bg': 'var(--color-text-highlight-bg-inverted)',
         }
       }
     },
