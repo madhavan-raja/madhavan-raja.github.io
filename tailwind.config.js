cc0bd4b05d81560c9da0e15643f47eceda7fd2ac
madhavan-raja <madhavanraja99@gmail.com>
2022-03-25 18:17:10 +0530

    Changed main font

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
   darkMode: 'class',
   theme: {
     extend: {
+      fontFamily: {
+           'sans': ['Poppins'],
+      },
       screens: {
         'print': {'raw': 'print'},
       },
