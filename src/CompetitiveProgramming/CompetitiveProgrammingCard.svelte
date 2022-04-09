c1436d1625f8a5bca0c631e04e74bae8d66a1562
madhavan-raja <madhavanraja99@gmail.com>
2022-04-09 22:06:56 +0530

    Made the competitive programming card consistent

--- src/CompetitiveProgramming/CompetitiveProgrammingCard.svelte
+++ src/CompetitiveProgramming/CompetitiveProgrammingCard.svelte
@@ -6,14 +6,19 @@
   export let ratingDescription;
 </script>
 
-<div>
-  <h3 class="text-item-heading">{platformName}</h3>
+<div class="group">
+  <a href={link}>
+    <h3 class="text-sm sm:text-lg font-semibold text-link">{platformName}</h3>
 
-  <a class="text-sm font-mono text-link print:text-vivid" href={link}>
-    {username}
+    <a
+      class="text-sm font-mono text-muted group-hover:text-link print:text-vivid"
+      href={link}
+    >
+      {username}
+    </a>
+    <span class="text-sm text-muted group-hover:text-link">
+      &bull; {rating} ({ratingDescription})
+    </span>
+    <div class="text-sm text-link hidden print:block">{link}</div>
   </a>
-  <span class="text-sm text-muted">
-    &bull; {rating} ({ratingDescription})
-  </span>
-  <div class="text-sm text-link hidden print:block">{link}</div>
 </div>
