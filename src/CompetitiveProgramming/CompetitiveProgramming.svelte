ee8321c11ae20ee34685e5de2b621a9fee0d4e6c
madhavan-raja <madhavanraja99@gmail.com>
2022-05-22 16:33:04 +0530

    Refactored

--- src/CompetitiveProgramming/CompetitiveProgramming.svelte
+++ src/CompetitiveProgramming/CompetitiveProgramming.svelte
@@ -1,23 +1,5 @@
 <script>
   import CompetitiveProgrammingCard from "./CompetitiveProgrammingCard.svelte";
-
-  // let platforms = [
-  //   {
-  //     platform: "CodeChef",
-  //     username: "flipped_flop",
-  //     link: "https://www.codechef.com/users/flipped_flop",
-  //     rating: 1862,
-  //     ratingDescription: "4★",
-  //   },
-  //   {
-  //     platform: "Codeforces",
-  //     username: "madhavan_raja",
-  //     link: "https://codeforces.com/profile/madhavan_raja",
-  //     rating: 1586,
-  //     ratingDescription: "Specialist",
-  //   },
-  // ];
-
   import client from "../sanity";
 
   const query = `*[_type == "competitiveProgramming"]{platform, rating, ratingDescription, username, link}`;
