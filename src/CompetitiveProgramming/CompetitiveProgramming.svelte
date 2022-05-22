cd277b8551c02ff9d2df763a514777cd3163005d
madhavan-raja <madhavanraja99@gmail.com>
2022-05-22 16:32:38 +0530

    Fetching Competitive Programming info from Sanity

--- src/CompetitiveProgramming/CompetitiveProgramming.svelte
+++ src/CompetitiveProgramming/CompetitiveProgramming.svelte
@@ -1,31 +1,31 @@
 <script>
   import CompetitiveProgrammingCard from "./CompetitiveProgrammingCard.svelte";
 
-  let platforms = [
-    {
-      platform: "CodeChef",
-      username: "flipped_flop",
-      link: "https://www.codechef.com/users/flipped_flop",
-      rating: 1862,
-      ratingDescription: "4★",
-    },
-    {
-      platform: "Codeforces",
-      username: "madhavan_raja",
-      link: "https://codeforces.com/profile/madhavan_raja",
-      rating: 1586,
-      ratingDescription: "Specialist",
-    },
-  ];
+  // let platforms = [
+  //   {
+  //     platform: "CodeChef",
+  //     username: "flipped_flop",
+  //     link: "https://www.codechef.com/users/flipped_flop",
+  //     rating: 1862,
+  //     ratingDescription: "4★",
+  //   },
+  //   {
+  //     platform: "Codeforces",
+  //     username: "madhavan_raja",
+  //     link: "https://codeforces.com/profile/madhavan_raja",
+  //     rating: 1586,
+  //     ratingDescription: "Specialist",
+  //   },
+  // ];
 
-  // import client from "../sanity";
+  import client from "../sanity";
 
-  // const query = `*[_type == "publication"]{name, authors, link, journal}`;
+  const query = `*[_type == "competitiveProgramming"]{platform, rating, ratingDescription, username, link}`;
 
-  // let platforms = [];
-  // client.fetch(query).then((res) => {
-  //   platforms = res;
-  // });
+  let platforms = [];
+  client.fetch(query).then((res) => {
+    platforms = res;
+  });
 </script>
 
 {#if platforms.length > 0}
