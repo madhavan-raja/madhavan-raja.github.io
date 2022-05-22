d453e5491cb222758bfb8df0f78a74c74946a468
madhavan-raja <madhavanraja99@gmail.com>
2022-05-22 16:31:27 +0530

    Refactored CP platform name variable

--- src/CompetitiveProgramming/CompetitiveProgramming.svelte
+++ src/CompetitiveProgramming/CompetitiveProgramming.svelte
@@ -3,20 +3,29 @@
 
   let platforms = [
     {
-      name: "CodeChef",
+      platform: "CodeChef",
       username: "flipped_flop",
       link: "https://www.codechef.com/users/flipped_flop",
       rating: 1862,
       ratingDescription: "4★",
     },
     {
-      name: "Codeforces",
+      platform: "Codeforces",
       username: "madhavan_raja",
       link: "https://codeforces.com/profile/madhavan_raja",
       rating: 1586,
       ratingDescription: "Specialist",
     },
   ];
+
+  // import client from "../sanity";
+
+  // const query = `*[_type == "publication"]{name, authors, link, journal}`;
+
+  // let platforms = [];
+  // client.fetch(query).then((res) => {
+  //   platforms = res;
+  // });
 </script>
 
 {#if platforms.length > 0}
@@ -25,7 +34,7 @@
 
     {#each platforms as platform}
       <CompetitiveProgrammingCard
-        platformName={platform.name}
+        platformName={platform.platform}
         link={platform.link}
         username={platform.username}
         rating={platform.rating}
